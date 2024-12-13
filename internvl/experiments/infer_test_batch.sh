c4a742a0e584655ce964cd484373aacb92e60f42
Zonghuan Li <z.li-25@tudelft.nl>
2024-12-13 18:35:50 +0100

    change for cluster run

--- internvl/experiments/infer_test_batch.sh
+++ internvl/experiments/infer_test_batch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #SBATCH --partition=insy,general # Request partition. Default is 'general'
-#SBATCH --qos=short         # Request Quality of Service. Default is 'short' (maximum run time: 4 hours)
-#SBATCH --time=4:00:00      # Request run time (wall-clock). Default is 1 minute
+#SBATCH --qos=medium         # Request Quality of Service. Default is 'short' (maximum run time: 4 hours)
+#SBATCH --time=10:00:00      # Request run time (wall-clock). Default is 1 minute
 #SBATCH --ntasks=1          # Request number of parallel tasks per job. Default is 1
 #SBATCH --cpus-per-task=2   # Request number of CPUs (threads) per task. Default is 1 (note: CPUs are always allocated to jobs per 2).
 #SBATCH --mem=32768          # Request memory (MB) per node. Default is 1024MB (1GB). For multiple tasks, specify --mem-per-cpu instead
@@ -21,7 +21,7 @@ previous=$(/usr/bin/nvidia-smi --query-accounted-apps='gpu_utilization,mem_utili
 # Uncomment these lines and adapt them to load the software that your job requires
 # module use /opt/insy/modulefiles          # Use DAIC INSY software collection
 # module load cuda/12.4 cudnn/12-8.9.1.23 # Load certain versions of cuda and cudnn
-srun bash infer_main.sh --model_name InternVL2-2B --visual_strat concat --task cgroup # Computations should be started with 'srun'. For example:
+srun bash infer_main.sh --model_name InternVL2-1B --visual_strat concat --task cgroup # Computations should be started with 'srun'. For example:
 
 # Measure GPU usage of your job (result)
 /usr/bin/nvidia-smi --query-accounted-apps='gpu_utilization,mem_utilization,max_memory_usage,time' --format='csv' | /usr/bin/grep -v -F "$previous"
