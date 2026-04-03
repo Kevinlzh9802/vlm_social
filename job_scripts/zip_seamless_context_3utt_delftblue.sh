4b40b71fe2ce70ba3aebe5d23ab5093031734b73
Zonghuan Li <zonghuan_li@outlook.com>
2026-04-03 20:23:27 +0200

    change sbatch specifics

--- job_scripts/zip_seamless_context_3utt_delftblue.sh
+++ job_scripts/zip_seamless_context_3utt_delftblue.sh
@@ -3,8 +3,8 @@
 #SBATCH --time=02:00:00
 #SBATCH --partition=compute-p1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=8G
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=2000M
 #SBATCH --mail-type=END
 #SBATCH --account=research-eemcs-insy
 #SBATCH --output=/scratch/zli33/slurm_outputs/vlm_social/slurm_%j.out
