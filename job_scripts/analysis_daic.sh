d990b074c4eecd624821ecba6ae7de787bd2821e
Zonghuan Li <zonghuan_li@outlook.com>
2026-04-24 14:03:36 +0200

    increase time

--- job_scripts/analysis_daic.sh
+++ job_scripts/analysis_daic.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name="analysis"
-#SBATCH --time=02:00:00
+#SBATCH --time=08:00:00
 #SBATCH --partition=insy,general
-#SBATCH --qos=short
+#SBATCH --qos=medium
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=8G
