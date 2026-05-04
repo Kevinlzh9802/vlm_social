460a384f842a29492d1db6494cbf0190116ba9c5
Zonghuan Li <zonghuan_li@outlook.com>
2026-05-04 17:17:30 +0200

    update qos

--- job_scripts/clip_statistics_daic.sh
+++ job_scripts/clip_statistics_daic.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name="clip_stats"
 #SBATCH --time=00:30:00
 #SBATCH --partition=insy,general
-#SBATCH --qos=medium
+#SBATCH --qos=short
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=8G
